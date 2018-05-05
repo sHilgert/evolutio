1c37949d6db1026cb82d0eaa6a40b4b7329c788d
Arthur Miranda Moreira <arthur_mmoreira@yahoo.com.br>
2018-05-05 20:58:12 -0300

    First version of menu and profile screen

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,5 +2,39 @@ class ProfileController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @skills = [
+      {
+        name: "Skill 1",
+        percentage: 25,
+        list: [
+          {
+            name: "Skill 11",
+            checked: true,
+            grade: 7.8
+          },
+          {
+            name: "Skill 12",
+            checked: true,
+            grade: 10.0
+          }
+        ]
+      },
+      {
+        name: "Skill 2",
+        percentage: 35,
+        list: [
+          {
+            name: "Skill 21",
+            checked: false,
+            grade: 2.8
+          },
+          {
+            name: "Skill 22",
+            checked: true,
+            grade: 8.4
+          }
+        ]
+      }
+    ]
   end
 end
