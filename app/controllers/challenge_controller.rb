c4202daec1f561d708c5ccaec38319ea219ad94a
Heitor Miranda <heitor.miranda@redealumni.com>
2018-05-06 07:42:26 -0300

    challenge cagado

--- app/controllers/challenge_controller.rb
+++ app/controllers/challenge_controller.rb
@@ -1,9 +1,4 @@
 class ChallengeController < ApplicationController
   def index
-    # array de hashes com {id: job_skill.id, skill: job_skill.skill.description}
-    current_user.job.job_skills
   end
 end
-
-
-{ competence_id: 1, skill_id: 2, job_id: 4 }
