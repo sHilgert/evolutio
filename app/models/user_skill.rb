0797157464ba7d1a4a482182e3eb483bc6e49573
Heitor Miranda <heitor.miranda@redealumni.com>
2018-05-06 05:58:57 -0300

    update user_skill

--- app/models/user_skill.rb
+++ app/models/user_skill.rb
@@ -1,11 +1,11 @@
 class UserSkill < ApplicationRecord
   belongs_to :user
   belongs_to :skill
-  belongs_to :competence
+  belongs_to :job_skill
 
   scope :job_skills, -> { where(skill_type: 'job') }
 
-  def self.from_competence(competence)
-    self.where(competence: competence)
-  end
+  # def self.from_competence(competence)
+  #   self.where(competence: competence)
+  # end
 end
