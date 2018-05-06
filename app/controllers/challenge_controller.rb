d604f9010877a80a36adb8ead17141bf1d1b48e3
Arthur Miranda Moreira <arthur_mmoreira@yahoo.com.br>
2018-05-06 07:49:21 -0300

    Add challenge screen with some bugs

--- app/controllers/challenge_controller.rb
+++ app/controllers/challenge_controller.rb
@@ -1,4 +1,16 @@
 class ChallengeController < ApplicationController
+  include ChallengeHelper
+
+  before_action :authenticate_user!
+
   def index
+    @leaders = [
+      "Yugue", "Ney", "Pufe", "Roger"
+    ].map(&:downcase)
+    @skills = get_job_skills(current_user)
+    @selectedSkills = []
+  end
+
+  def create
   end
 end
--- app/helpers/challenge_helper.rb
+++ app/helpers/challenge_helper.rb
@@ -1,2 +1,6 @@
 module ChallengeHelper
+
+  def get_job_skills(user)
+    user.job.job_skills.collect {|js| { id: js.skill.id, skill: js.skill.description.downcase } }
+  end
 end
