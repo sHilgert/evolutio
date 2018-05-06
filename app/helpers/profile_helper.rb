8db0e51d6cd60fd89c3e59bc35f0391e2ffddec6
Heitor Miranda <heitor.miranda@redealumni.com>
2018-05-06 01:58:24 -0300

    merge bla

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,5 +1,4 @@
 module ProfileHelper
-
   def round_five(number)
     (number / 5).round * 5
   end
--- app/models/level.rb
+++ app/models/level.rb
@@ -1,2 +1,5 @@
 class Level < ApplicationRecord
+  def self.get_from_percentage(percentage)
+    Level.where("minimum_percentage <= ? AND maximum_percentage >= ?", percentage, percentage).first
+  end
 end
