af1681c1ef5a0633b186719ef0c871f596c8a740
sergio.hilgert <hilgertfacul@gmail.com>
2018-05-06 07:10:47 -0300

    VAMO QUE VAMO

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  belongs_to :job
+  belongs_to :job, optional: true
   has_one :leader, class_name: 'User'
   has_many :user_skills
 
@@ -9,14 +9,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: %i[slack]
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+    if user.present?
       user.email = "#{auth.info.user}@bla.com"
       user.password = Devise.friendly_token[0,20]
-      #user.name = auth.info.name   # assuming the user model has a name
-      #user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails,
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+      user.name = auth.info.name   # assuming the user model has a name
+      user.avatar = auth.info.image # assuming
+      user.save
+      user
+    else
+      user = User.create(email: "#{auth.info.user}@bla.com", password: Devise.friendly_token[0,20], name: auth.info.name, avatar: auth.info.image)
+      user
     end
   end
 
@@ -25,7 +28,7 @@ class User < ApplicationRecord
     user_skills = self.user_skills.job_skills.from_competence(competence)
 
     user_skills.each do |us|
-      
+
     end
   end
 end
